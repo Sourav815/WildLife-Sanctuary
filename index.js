b9e68dd8175e72ccd55e38195e7e5fe64e2c4e7b
Sourav <souravbhunia162@gmail.com>
2023-05-15 01:58:45 +0530

    Initially Added to Github

---
+++ index.js
@@ -0,0 +1,13 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+window.addEventListener('scroll',()=>{
+    text.style.marginTop = window.scrollY * 2.5 + 'px';
+    leaf.style.left = window.scrollY * 1.5 + 'px';
+    leaf.style.top = window.scrollY * -1.5 + 'px';
+    hill5.style.left = window.scrollY * 1.5 + 'px';
+    hill4.style.left = window.scrollY * -1.5 + 'px';
+})
